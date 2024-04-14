9773f3fd049cc08afd319814f9b5e1290fea9556
Andrewbandrew05 <andrewhrissler@gmail.com>
2024-04-14 19:41:33 -0400

    I FIXED THE WHITE BAR AT THE BOTTOM OF THE SCREENNNNNN

--- JavaScript/Universal.js
+++ JavaScript/Universal.js
@@ -135,25 +135,6 @@ function arrange_Links_In_LinkHolders()
         if(num_Links_In_Row > 0) {
             calculate_Margins(total_Link_Width_In_Row, num_Links_In_Row, total_Links_Processed, linkHolder);     
         }
-        if(linkHolder.classList.contains("footer")) {
-            //document.getElementById("main_Body").style.height = document.getElementById("lastElement").getBoundingClientRect().top + document.getElementById("lastElement").getBoundingClientRect().height;
-            /**document.body.style.height = linkHolder.getBoundingClientRect().top + linkHolder.getBoundingClientRect().height;
-            document.getElementById("main_Body").style.height = document.body.getBoundingClientRect().height - document.getElementById("parallax_Effect_Creator").getBoundingClientRect().height;
-            **/
-            document.body.style.height = document.getElementById("main_Body").getBoundingClientRect().height+document.getElementById("parallax_Effect_Creator").getBoundingClientRect().height;
-            console.log("mainbody stufff");
-            console.log(document.body.style.height);
-            console.log(document.getElementById("main_Body").getBoundingClientRect().height);
-            console.log(document.getElementById("parallax_Effect_Creator").getBoundingClientRect().height);
-            /**console.log(document.getElementById("lastElement").getBoundingClientRect().top);
-            console.log(linkHolder.getBoundingClientRect().top);
-            console.log(linkHolder.getBoundingClientRect().height);
-            console.log(document.body.getBoundingClientRect().height);
-            console.log(document.getElementById("parallax_Effect_Creator").getBoundingClientRect().height);
-            console.log(document.body.style.height - document.getElementById("parallax_Effect_Creator").getBoundingClientRect().height);
-            console.log(document.getElementById("main_Body").style.height);
-            **/
-        }
     }
 }
 //need to make it check to make sure one does not exceend parent size
@@ -337,8 +318,8 @@ function position_Components(images_Have_Loaded){
             html_Element.onload=function(){position_Image(html_Element);}
         }
     }
-    //calculations for body sizes in arrangelinksinholders to help with efficiency
     arrange_Links_In_LinkHolders();
+    document.body.style.height=convert_Dimension_To_Int(window.getComputedStyle(document.getElementById("main_Body"), null).getPropertyValue("height"))+convert_Dimension_To_Int(window.getComputedStyle(document.getElementsByClassName("parallax_Effect_Creator")[0], null).getPropertyValue("height"))+'px';
 }
 //inital setup
 position_Components(false);
